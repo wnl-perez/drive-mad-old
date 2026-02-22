6627de8b163f1d48a508fa53bb583dd1ae95ab0c
wnl-perez <cecil0620perez@gmail.com>
2026-02-22 16:59:08 -0600

    Create Poki_tricker.js

---
+++ Poki_tricker.js
@@ -0,0 +1,15 @@
+window.assgdd = {
+  ancestorOrigins: {
+    0: "https://games.poki.com",
+    1: "https://poki.com"
+  },
+  href: "https://fake.poki-gdn.com/index.html",
+  origin: "https://fake.poki-gdn.com",
+  protocol: "https:",
+  host: "fake.poki-gdn.com",
+  hostname: "fake.poki-gdn.com",
+  port: "",
+  pathname: "/index.html",
+  search: "",
+  hash: ""
+};
